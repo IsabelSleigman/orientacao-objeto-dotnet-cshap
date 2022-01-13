d6d1952e38b4fae5da857c6ad962e07820c78162
isabelsleigman <sleimusic@gmail.com>
2022-01-13 11:57:45 -0300

    configuração e primeiros passos

---
+++ ExemploPOO/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ExemploPOO.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Aprensentar()
+        {
+            Console.WriteLine($"Olá, Meu nome é {Nome} e tenho {Idade} anos");
+        }
+
+    }
+}
---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Isabel";
+            p1.Idade = 30;
+
+            p1.Aprensentar();
+        }
+    }
+}
